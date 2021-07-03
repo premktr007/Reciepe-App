c5777b4486d10646abcd1ccc099aa5dbbb71d8f7
premktr007 <prem.ktr007@gmail.com>
2021-07-03 21:02:27 +0530

    fixed the header for mobile screens

--- src/app/recipes/recipe.module.ts
+++ src/app/recipes/recipe.module.ts
@@ -7,7 +7,6 @@ import { RecipeEditComponent } from "./recipe-edit/recipe-edit.component";
 import { RecipeItemComponent } from "./recipe-list/recipe-item/recipe-item.component";
 import { RecipeListComponent } from "./recipe-list/recipe-list.component";
 import { RecipeStartComponent } from "./recipe-start/recipe-start.component";
-import { RecipeService } from "./recipe.service";
 import { RecipesComponent } from "./recipes.component";
 import { recipesRoutingModule } from "./recipes.routing";
 
